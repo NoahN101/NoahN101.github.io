1d9d5a872e433888a4dfdaa3a2da225fd435375e
NoahN101 <naquin585@wearescpps.org>
2025-09-15 14:09:44 +0000

    platformer

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -7,8 +7,8 @@ $(function () {
   function setup() {
     if (firstTimeSetup) {
       halleImage = document.getElementById("player");
-      projectileImage = document.getElementById("projectile");
-      cannonImage = document.getElementById("cannon");
+      projectileImage = document.getElementById("water");
+      cannonImage = document.getElementById("waterGun");
       $(document).on("keydown", handleKeyDown);
       $(document).on("keyup", handleKeyUp);
       firstTimeSetup = false;
@@ -27,7 +27,7 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    //toggleGrid();
+    toggleGrid();
 
 
     // TODO 2 - Create Platforms
@@ -49,15 +49,17 @@ createPlatform(1050, 625, 60, 10, "red");
 createPlatform(1225, 250, 60, 10, "red");
 createPlatform(1225, 500, 60, 10, "red");
     // TODO 3 - Create Collectables
-createCollectable("waterBalloonRed", 175, 225, 0.5, 0.7);
-createCollectable("waterBalloonBlue", 525, 225, 0.5, 0.7);
-createCollectable("waterBalloonPurple", 525, 525, 0.5, 0.7);
-createCollectable("waterBallonGreen", 1050, 400, 0.5, 0.7);
-    // TODO 4 - Create Cannons
-createCannon("top", 275, 2000);
+//createCollectable("red", 175, 225);
+//createCollectable("blue", 525, 225);
+//createCollectable("yellow", 525, 525);
+//createCollectable("green", 1050, 400);
+//     // TODO 4 - Create Cannons
+createwaterGun("top", 275, 2000);
 createCannon("bottom", 625, 1750);
 createCannon("top", 975, 1500);
 createCannon("bottom", 1125, 1250);
+createCannon("left", 200, 1500);
+createCannon("right", 500, 1500)
     //////////////////////////////////
     // ONLY CHANGE ABOVE THIS POINT //
     //////////////////////////////////
